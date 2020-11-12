a99bd4cea57e898bda21985dbbdabef20bbcf208
markstefanovic <mark.stefanovic@gmail.com>
2020-11-12 12:45:29 -0800

    export HiveSqlTableAdapter

--- py_db_adapter/adapter/sql_adapter/__init__.py
+++ py_db_adapter/adapter/sql_adapter/__init__.py
@@ -1,4 +1,5 @@
 from py_db_adapter.adapter.sql_adapter.sql_column_adapter.column_sql_adapter import *
 from py_db_adapter.adapter.sql_adapter.sql_column_adapter import *
 from py_db_adapter.adapter.sql_adapter.sql_table_adapter import *
+from py_db_adapter.adapter.sql_adapter.hive_sql_adapter import *
 from py_db_adapter.adapter.sql_adapter.postgres_sql_adapter import *
