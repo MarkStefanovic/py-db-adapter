aa1607861536395464d431a4f12efde915b0fb89
MarkStefanovic <IUhwaeiurhzx2n>
2021-05-22 16:21:18 -0700

    add additional tests for change tracking

--- py_db_adapter/domain/unique_constraint.py
+++ py_db_adapter/domain/unique_constraint.py
@@ -4,7 +4,7 @@ import typing
 
 @dataclasses.dataclass(frozen=True)
 class UniqueContraint:
-    columns: typing.List[str]
+    columns: typing.Tuple[str, ...]
 
     def definition(self, *, wrapper: typing.Callable[[str], str]) -> str:
         return f"UNIQUE ({', '.join(wrapper(col) for col in self.columns)})"
