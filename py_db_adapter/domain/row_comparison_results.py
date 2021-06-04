139f8cd51ce0651974274c4617dc828d4dd60d5a
markstefanovic <markstefanovic@hotmail.com>
2021-06-04 10:43:30 -0700

    add error to row_comparison_result

--- py_db_adapter/domain/row_comparison_results.py
+++ py_db_adapter/domain/row_comparison_results.py
@@ -1,7 +1,10 @@
 import dataclasses
+import datetime
 import decimal
 
 __all__ = ("RowComparisonResult",)
+
+import typing
 
 
 @dataclasses.dataclass(frozen=True, eq=True)
@@ -19,3 +22,34 @@ class RowComparisonResult:
     stale_rows: int
     stale_row_examples: str
     pct_stale: decimal.Decimal
+    ts: datetime.datetime
+    error_message: typing.Optional[str]
+    traceback: typing.Optional[typing.Tuple[str, ...]]
+
+    @property
+    def is_error(self) -> bool:
+        return self.error_message is not None
+
+    @property
+    def is_success(self) -> bool:
+        return self.error_message is None
+
+    def __str__(self) -> str:
+        return f"""{self.__class__.__name__}
+        ts: {self.ts.strftime("%Y-%m-%d %H:%M:%S.%f")}
+        src_schema: {self.src_schema}
+        src_table: {self.src_table}
+        dest_schema: {self.dest_schema}
+        dest_table: {self.dest_table}
+        missing_rows: {self.missing_rows}
+        missing_row_examples: {self.missing_row_examples}
+        pct_missing: {self.pct_missing * 100:.0f}%
+        extra_rows: {self.extra_rows}
+        extra_row_examples: {self.extra_row_examples}
+        pct_extra: {self.pct_extra * 100:.0f}%
+        stale_rows: {self.stale_rows}
+        stale_row_examples: {self.stale_row_examples}
+        pct_stale: {self.pct_stale * 100:.0f}%
+        error_message: {self.error_message}
+        traceback: {self.traceback}
+        """
--- py_db_adapter/service/compare_rows.py
+++ py_db_adapter/service/compare_rows.py
@@ -1,4 +1,5 @@
 import dataclasses
+import datetime
 import decimal
 import itertools
 import pathlib
@@ -28,118 +29,142 @@ def compare_rows(
     max_examples: int = 10,
     # fmt: on
 ) -> domain.RowComparisonResult:
-    if pk_cols is None:
-        pk_cols = get_pks(
-            pk_cols=pk_cols,
-            src_cur=src_cur,
-            dest_cur=dest_cur,
-            src_schema_name=src_schema_name,
-            src_table_name=src_table_name,
-            dest_schema_name=dest_schema_name,
-            dest_table_name=dest_table_name,
-        )
-
-    src_table = adapter.inspect_table(
-        cur=src_cur,
-        table_name=src_table_name,
-        schema_name=src_schema_name,
-        pk_cols=pk_cols,
-        include_cols=None,
-        cache_dir=cache_dir,
-    )
-
-    dest_table = adapter.inspect_table(
-        cur=dest_cur,
-        table_name=dest_table_name,
-        schema_name=dest_schema_name,
-        pk_cols=pk_cols,
-        include_cols=None,
-        cache_dir=cache_dir,
-    )
-
-    pks = set(pk_cols)
-
-    src_table = dataclasses.replace(
-        src_table,
-        primary_key=domain.PrimaryKey(
-            schema_name=src_schema_name,
-            table_name=src_table_name,
-            columns=tuple(sorted(pks)),
-        ),
-    )
-
-    dest_table = dataclasses.replace(
-        dest_table,
-        primary_key=domain.PrimaryKey(
-            schema_name=dest_schema_name,
-            table_name=dest_table_name,
-            columns=tuple(sorted(pks)),
-        ),
-    )
-
-    if compare_cols is None:
-        src_cols = src_table.non_pk_column_names
-        dest_cols = dest_table.non_pk_column_names
-        compare_cols = src_cols & dest_cols
-
-    src_rows = src_db_adapter.table_keys(
-        cur=src_cur,
-        table=src_table,
-        additional_cols=compare_cols,
-    )
-
-    dest_rows = dest_db_adapter.table_keys(
-        cur=dest_cur,
-        table=dest_table,
-        additional_cols=compare_cols,
-    )
-
-    diff: domain.RowDiff = domain.compare_rows(
-        src_rows=src_rows,
-        dest_rows=dest_rows,
-        key_cols=pks,
-        compare_cols=compare_cols,
-    )
-
-    extra_rows = diff.rows_deleted
-    missing_rows = diff.rows_added
-    stale_rows = diff.rows_updated
-
-    extra_pct = decimal.Decimal(
-        format(extra_rows.row_count / src_rows.row_count, ".2f")
-    )
-    missing_pct = decimal.Decimal(
-        format(missing_rows.row_count / src_rows.row_count, ".2f")
-    )
-    stale_pct = decimal.Decimal(
-        format(stale_rows.row_count / src_rows.row_count, ".2f")
-    )
-
-    extra_examples = rows_to_examples(
-        rows=extra_rows, pk_cols=pks, max_examples=max_examples
-    )
-    missing_examples = rows_to_examples(
-        rows=missing_rows, pk_cols=pks, max_examples=max_examples
-    )
-    stale_examples = rows_to_examples(
-        rows=stale_rows, pk_cols=pks, max_examples=max_examples
-    )
-
-    return domain.RowComparisonResult(
+    result = domain.RowComparisonResult(
         src_schema=src_schema_name,
         src_table=src_table_name,
         dest_schema=dest_schema_name,
         dest_table=dest_table_name,
-        missing_rows=diff.rows_added.row_count,
-        missing_row_examples=missing_examples,
-        pct_missing=missing_pct,
-        extra_rows=diff.rows_deleted.row_count,
-        extra_row_examples=extra_examples,
-        pct_extra=extra_pct,
-        stale_rows=diff.rows_updated.row_count,
-        stale_row_examples=stale_examples,
-        pct_stale=stale_pct,
+        missing_rows=0,
+        missing_row_examples="",
+        pct_missing=decimal.Decimal("0"),
+        extra_rows=0,
+        extra_row_examples="",
+        pct_extra=decimal.Decimal("0"),
+        stale_rows=0,
+        stale_row_examples="",
+        pct_stale=decimal.Decimal("0"),
+        ts=datetime.datetime.now(),
+        error_message=None,
+        traceback=None,
     )
+    try:
+        if pk_cols is None:
+            pk_cols = get_pks(
+                src_cur=src_cur,
+                dest_cur=dest_cur,
+                src_schema_name=src_schema_name,
+                src_table_name=src_table_name,
+                dest_schema_name=dest_schema_name,
+                dest_table_name=dest_table_name,
+            )
+
+        src_table = adapter.inspect_table(
+            cur=src_cur,
+            table_name=src_table_name,
+            schema_name=src_schema_name,
+            pk_cols=pk_cols,
+            include_cols=None,
+            cache_dir=cache_dir,
+        )
+
+        dest_table = adapter.inspect_table(
+            cur=dest_cur,
+            table_name=dest_table_name,
+            schema_name=dest_schema_name,
+            pk_cols=pk_cols,
+            include_cols=None,
+            cache_dir=cache_dir,
+        )
+
+        pks = set(pk_cols)
+
+        src_table = dataclasses.replace(
+            src_table,
+            primary_key=domain.PrimaryKey(
+                schema_name=src_schema_name,
+                table_name=src_table_name,
+                columns=tuple(sorted(pks)),
+            ),
+        )
+
+        dest_table = dataclasses.replace(
+            dest_table,
+            primary_key=domain.PrimaryKey(
+                schema_name=dest_schema_name,
+                table_name=dest_table_name,
+                columns=tuple(sorted(pks)),
+            ),
+        )
+
+        if compare_cols is None:
+            src_cols = src_table.non_pk_column_names
+            dest_cols = dest_table.non_pk_column_names
+            compare_cols = src_cols & dest_cols
+
+        src_rows = src_db_adapter.table_keys(
+            cur=src_cur,
+            table=src_table,
+            additional_cols=compare_cols,
+        )
+
+        dest_rows = dest_db_adapter.table_keys(
+            cur=dest_cur,
+            table=dest_table,
+            additional_cols=compare_cols,
+        )
+
+        diff: domain.RowDiff = domain.compare_rows(
+            src_rows=src_rows,
+            dest_rows=dest_rows,
+            key_cols=pks,
+            compare_cols=compare_cols,
+        )
+
+        extra_rows = diff.rows_deleted
+        missing_rows = diff.rows_added
+        stale_rows = diff.rows_updated
+
+        if src_rows.row_count:
+            extra_pct = decimal.Decimal(
+                format(extra_rows.row_count / src_rows.row_count, ".2f")
+            )
+            missing_pct = decimal.Decimal(
+                format(missing_rows.row_count / src_rows.row_count, ".2f")
+            )
+            stale_pct = decimal.Decimal(
+                format(stale_rows.row_count / src_rows.row_count, ".2f")
+            )
+        else:
+            extra_pct = decimal.Decimal("1")
+            missing_pct = decimal.Decimal("0")
+            stale_pct = decimal.Decimal("0")
+
+        extra_examples = rows_to_examples(
+            rows=extra_rows, pk_cols=pks, max_examples=max_examples
+        )
+        missing_examples = rows_to_examples(
+            rows=missing_rows, pk_cols=pks, max_examples=max_examples
+        )
+        stale_examples = rows_to_examples(
+            rows=stale_rows, pk_cols=pks, max_examples=max_examples
+        )
+        result = dataclasses.replace(
+            result,
+            missing_rows=diff.rows_added.row_count,
+            missing_row_examples=missing_examples,
+            pct_missing=missing_pct,
+            extra_rows=diff.rows_deleted.row_count,
+            extra_row_examples=extra_examples,
+            pct_extra=extra_pct,
+            stale_rows=diff.rows_updated.row_count,
+            stale_row_examples=stale_examples,
+            pct_stale=stale_pct,
+        )
+    except Exception as e:
+        tb = domain.exceptions.parse_traceback(e)
+        result = dataclasses.replace(result, error_message=str(e), traceback=tb)
+    finally:
+        return result
 
 
 def get_pks(
@@ -175,13 +200,16 @@ def get_pks(
 def rows_to_examples(
     rows: domain.Rows, pk_cols: typing.Set[str], max_examples: int
 ) -> str:
-    pks: typing.List[typing.Dict[str, typing.Any]] = rows.subset(pk_cols).as_dicts()
-    if pks:
-        prefix = "(" + ", ".join(pks[0].keys()) + "): "
-        examples = [
-            "(" + ", ".join(str(c) for c in row.values()) + ")"
-            for row in sorted(pks, key=lambda d: tuple(d.values()))[:max_examples]
-        ]
-        return prefix + ", ".join(str(x) for x in examples)
-    else:
+    if rows.is_empty:
         return ""
+    else:
+        pks: typing.List[typing.Dict[str, typing.Any]] = rows.subset(pk_cols).as_dicts()
+        if pks:
+            prefix = "(" + ", ".join(pks[0].keys()) + "): "
+            examples = [
+                "(" + ", ".join(str(c) for c in row.values()) + ")"
+                for row in sorted(pks, key=lambda d: tuple(d.values()))[:max_examples]
+            ]
+            return prefix + ", ".join(str(x) for x in examples)
+        else:
+            return ""
